7a5e8f8aeb5fe6ade59ec4d1241101d657854424
AgustinVillavicencio <villavicencio.agustin.17@gmail.com>
2024-08-19 21:22:21 -0300

    agrego a la bd: sucursales y imagensucursales

--- src/main/java/com/example/buensaborback/business/facade/ImagenSucursalFacade.java
+++ src/main/java/com/example/buensaborback/business/facade/ImagenSucursalFacade.java
@@ -1,7 +1,8 @@
 package com.example.buensaborback.business.facade;
 
+import com.example.buensaborback.business.facade.Base.BaseFacade;
+import com.example.buensaborback.domain.entities.ImagenSucursal;
 import org.springframework.web.multipart.MultipartFile;
-
 
 public interface ImagenSucursalFacade {
     String uploadImages(MultipartFile file, Long idSucursal);
--- src/main/java/com/example/buensaborback/presentation/rest/ImagenSucursalController.java
+++ src/main/java/com/example/buensaborback/presentation/rest/ImagenSucursalController.java
@@ -1,8 +1,9 @@
 package com.example.buensaborback.presentation.rest;
 
-import com.example.buensaborback.business.facade.ImagenPersonaFacade;
 import com.example.buensaborback.business.facade.ImagenSucursalFacade;
+import com.example.buensaborback.domain.entities.ImagenSucursal;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,6 +14,7 @@ public class ImagenSucursalController {
 
     @Autowired
     private ImagenSucursalFacade imagenSucursalFacade;
+
     @PostMapping("/uploads/{idSucursal}")
     public String uploadImages(@RequestParam("uploads") MultipartFile file, @PathVariable Long idSucursal) {
         return imagenSucursalFacade.uploadImages(file, idSucursal);
